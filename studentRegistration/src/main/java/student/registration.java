c1ba074d3cb279276d661fcec23aed879626248b
suman ghimire <ghimiresuman409@gmail.com>
2020-12-31 13:50:10 +0545

    shortcut bug fixed

--- studentRegistration/src/main/java/student/registration.java
+++ studentRegistration/src/main/java/student/registration.java
@@ -196,9 +196,16 @@ public class registration extends javax.swing.JFrame {
             Class[] types = new Class [] {
                 java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
             };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false
+            };
 
             public Class getColumnClass(int columnIndex) {
                 return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
             }
         });
         display.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -549,13 +556,12 @@ public class registration extends javax.swing.JFrame {
 
     private void refreshActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_refreshActionPerformed
         // TODO add your handling code here:
+        display.getSelectionModel().clearSelection();
         txtRollNo.setText("");
         txtName.setText("");
         txtMobile.setText("");
         txtCourse.setText("");
         txtRollNo.requestFocus();
-        
-        display.getSelectionModel().clearSelection();
     }//GEN-LAST:event_refreshActionPerformed
 
     private void txtRollNoKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtRollNoKeyPressed
